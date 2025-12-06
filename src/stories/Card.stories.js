5aacc9bfed185b44874d5ab4ce08bd29df1dfa1b
foziakassa <foziakassa19@gmail.com>
2025-12-06 05:49:54 +0300

    test

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from "react";
 import SportsSection from "./Card"; // Adjust path
 import "../index.css";
@@ -16,5 +17,21 @@ export default {
 export const Default = {
   args: {
     color: "red",
+  },
+};
+export const LoggedIn = {
+  play: async ({ canvasElement }) => {
+    // eslint-disable-next-line no-undef
+    const canvas = within(canvasElement);
+    const loginButton = canvas.getByRole("button", { name: /Log in/i });
+    // eslint-disable-next-line no-undef
+    await expect(loginButton).toBeInTheDocument();
+    // eslint-disable-next-line no-undef
+    await userEvent.click(loginButton);
+    await expect(loginButton).not.toBeInTheDocument();
+    ///fghbhdgjb
+
+    const logoutButton = canvas.getByRole("button", { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
   },
 };
